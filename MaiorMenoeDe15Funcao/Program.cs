c6e131b4c144ef972df558dfbc50278a3808b37c
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-31 21:04:17 -0300

    metodos com retornos e parametros

--- MaiorMenoeDe15Funcao/Program.cs
+++ MaiorMenoeDe15Funcao/Program.cs
@@ -2,62 +2,83 @@
 {
     private static void Main(string[] args)
     {
-        double numero = 0;
-        double[] vetor = new double[5];
-        double maior = 0;
-        double menor = 0;
+        int op = 1;
 
+        do
+        {
+            Console.WriteLine(">> Sequencia de números <<");
+            double[] v = EscolheNumeros();
+            Imprime(EscolheMaior(v), EscolheMenor(v));
+
+            Console.WriteLine("Gostaria de informar uma nova sequencia de números?\n[1 - sim]\n[2 - não (Sair)]");
+            if (!int.TryParse(Console.ReadLine(), out op))
+            {
+                Console.WriteLine("Valor inválido, digite uma opção válida");
+                continue;
+            }
+            if(op == 2)
+            {
+                op = 2;
+                Console.WriteLine("Saindo..");
+            }
+            if(op < 1 || op > 2)
+            {
+                Console.WriteLine("Valor inválido, digite uma opção válida");
+                continue;
+            }
+         } while (op == 1);
+    }
+
+    static double[] EscolheNumeros()
+    {
+        double[] vetor = new double[5];
         for (int i = 0; i < 5; i++)
         {
             Console.WriteLine("Insira o numero");
-            vetor[i] = double.Parse(Console.ReadLine());
-
-        }
-
-        escolheMaior();
-        escolheMenor();
- //       imprime();
-
-
-        //maior
-        double escolheMaior()
-        {
-            double maior = vetor[0];
-
-            for (int i = 0; i < 5; i++)
+            if(!double.TryParse(Console.ReadLine(), out vetor[i]))
             {
-                if (maior < vetor[i])
-                {
-                    maior = vetor[i];
-                }
+                Console.WriteLine("Valor inválido, digite um valor válido.");
+                continue;
             }
-            Console.WriteLine("Maior numero: " + maior );
-            return maior;
+
+             
         }
+            return vetor;
+    }
 
+    static double EscolheMaior(double[] vetor)
+    {
+        double maior = vetor[0];
 
-        //menor
-        double escolheMenor()
+        for (int i = 0; i < 5; i++)
         {
-            double menor = vetor[0];
-
-            for (int i = 0; i < 5; i++)
+            if (maior < vetor[i])
             {
-                if (menor > vetor[i])
-                {
-                    menor = vetor[i];
-                }
+                maior = vetor[i];
             }
-            Console.WriteLine("Menor numero: " + menor);
-            return menor;
         }
+        return maior;
+    }
 
-  /*
-        void imprime()
+    static double EscolheMenor(double[] vetor)
+    {
+        double menor = vetor[0];
+
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine("Maior numero: " + maior + ", e menor : " + menor);
+            if (menor > vetor[i])
+            {
+                menor = vetor[i];
+            }
         }
-*/
+        return menor;
+    }
 
+    static void Imprime(double maior, double menor)
+    {
+        Console.WriteLine("Maior numero: " + maior + ", e menor : " + menor);
     }
 }
+
+
+
